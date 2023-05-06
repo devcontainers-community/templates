b918716d83a72e3b553fb150195ed2e439621a26
eitsupi <ts1s1andn@gmail.com>
2023-05-06 13:37:03 +0900

    refactor: make linter happy

--- test.sh
+++ test.sh
@@ -20,10 +20,10 @@ cp -R "src/${TEMPLATE_ID}" "${SRC_DIR}"
 pushd "${SRC_DIR}"
 
 # Configure templates only if `devcontainer-template.json` contains the `options` property.
-OPTION_PROPERTY=( $(jq -r '.options' devcontainer-template.json) )
+OPTION_PROPERTY="$(jq -r '.options' devcontainer-template.json)"
 
 if [ "${OPTION_PROPERTY}" != "" ] && [ "${OPTION_PROPERTY}" != "null" ] ; then
-    OPTIONS=( $(jq -r '.options | keys[]' devcontainer-template.json) )
+    mapfile -t OPTIONS < <(jq -r '.options | keys[]' devcontainer-template.json)
 
     if [ "${OPTIONS[0]}" != "" ] && [ "${OPTIONS[0]}" != "null" ] ; then
         echo "(!) Configuring template options for '${TEMPLATE_ID}'"
@@ -50,9 +50,9 @@ TEST_DIR="test/${TEMPLATE_ID}"
 if [ -d "${TEST_DIR}" ] ; then
     echo "(*) Copying test folder"
     DEST_DIR="${SRC_DIR}/test-project"
-    mkdir -p ${DEST_DIR}
-    cp -Rp ${TEST_DIR}/* ${DEST_DIR}
-    cp test/test-utils/test-utils.sh ${DEST_DIR}
+    mkdir -p "${DEST_DIR}"
+    cp -Rp "${TEST_DIR}"/* "${DEST_DIR}"
+    cp test/test-utils/test-utils.sh "${DEST_DIR}"
 fi
 
 export DOCKER_BUILDKIT=1
@@ -61,13 +61,14 @@ npm install -g @devcontainers/cli
 
 echo "Building Dev Container"
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer up --id-label ${ID_LABEL} --workspace-folder "${SRC_DIR}"
+devcontainer up --id-label "${ID_LABEL}" --workspace-folder "${SRC_DIR}"
 
 echo "Running Smoke Test"
 
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
+# shellcheck disable=SC2016
+devcontainer exec --workspace-folder "${SRC_DIR}" --id-label "${ID_LABEL}" /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
 
 # Clean up
-docker rm -f $(docker container ls -f "label=${ID_LABEL}" -q)
+docker rm -f "$(docker container ls -f "label=${ID_LABEL}" -q)"
 rm -rf "${SRC_DIR}"
