ba4ffdad6d6ae12e1cbe80f0171385caf8802cc4
eitsupi <eitsupi@users.noreply.github.com>
2023-05-06 04:46:54 +0000

    [MegaLinter] Apply linters fixes

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ TEMPLATE_ID="$1"
 
 set -e
 
-if [ "${TEMPLATE_ID}" = "" ] ; then
+if [ "${TEMPLATE_ID}" = "" ]; then
     echo "(!) Missing Template Id argument. Please provide a template id."
     exit 1
 fi
@@ -22,17 +22,16 @@ pushd "${SRC_DIR}"
 # Configure templates only if `devcontainer-template.json` contains the `options` property.
 OPTION_PROPERTY="$(jq -r '.options' devcontainer-template.json)"
 
-if [ "${OPTION_PROPERTY}" != "" ] && [ "${OPTION_PROPERTY}" != "null" ] ; then
+if [ "${OPTION_PROPERTY}" != "" ] && [ "${OPTION_PROPERTY}" != "null" ]; then
     mapfile -t OPTIONS < <(jq -r '.options | keys[]' devcontainer-template.json)
 
-    if [ "${OPTIONS[0]}" != "" ] && [ "${OPTIONS[0]}" != "null" ] ; then
+    if [ "${OPTIONS[0]}" != "" ] && [ "${OPTIONS[0]}" != "null" ]; then
         echo "(!) Configuring template options for '${TEMPLATE_ID}'"
-        for OPTION in "${OPTIONS[@]}"
-        do
+        for OPTION in "${OPTIONS[@]}"; do
             OPTION_KEY="\${templateOption:$OPTION}"
             OPTION_VALUE=$(jq -r ".options | .${OPTION} | .default" devcontainer-template.json)
 
-            if [ "${OPTION_VALUE}" = "" ] || [ "${OPTION_VALUE}" = "null" ] ; then
+            if [ "${OPTION_VALUE}" = "" ] || [ "${OPTION_VALUE}" = "null" ]; then
                 echo "Template '${TEMPLATE_ID}' is missing a default value for option '${OPTION}'"
                 exit 1
             fi
@@ -47,7 +46,7 @@ fi
 popd
 
 TEST_DIR="test/${TEMPLATE_ID}"
-if [ -d "${TEST_DIR}" ] ; then
+if [ -d "${TEST_DIR}" ]; then
     echo "(*) Copying test folder"
     DEST_DIR="${SRC_DIR}/test-project"
     mkdir -p "${DEST_DIR}"
